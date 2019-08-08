43407f61f9a533d19397469ebdc0bb0915d1266a
Aayush Mamgain <aayush@flixstock.com>
2019-08-08 12:24:51 +0530

    host variable

--- src/app/_services/chat.service.ts
+++ src/app/_services/chat.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+let host = 'http://127.0.0.1:2000';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -19,26 +20,26 @@ export class ChatService {
     console.log('chat server AI');
   }
 
-  createChatId(userId) { return this.http.get('http://127.0.0.1:2000/api/createChatId?userId='+userId) };
-  createUserId() { return this.http.get('http://127.0.0.1:2000/api/createUserId') };
+  createChatId(userId) { return this.http.get(host + '/api/createChatId?userId='+userId) };
+  createUserId() { return this.http.get(host + '/api/createUserId') };
 
   getChatMessages(data) { 
-    return this.http.get('http://127.0.0.1:2000/api/getItem?userId='+data.userId+'&chatId='+data.chatId) 
+    return this.http.get(host + '/api/getItem?userId='+data.userId+'&chatId='+data.chatId) 
   }
   pushMessageToChat(data) {
-    return this.http.post('http://127.0.0.1:2000/api/pushMessageToChat?userId='+data.userId+'&chatId='+data.chatId, {message: data.message}, httpOptions);
+    return this.http.post(host + '/api/pushMessageToChat?userId='+data.userId+'&chatId='+data.chatId, {message: data.message}, httpOptions);
   }
 
-  getOnlineUsers() { return this.http.get('http://127.0.0.1:2000/api/getOnlineUsers') };
-  getUserConnectedChats(userId) { return this.http.get('http://127.0.0.1:2000/api/getUserConnectedChats?userId='+userId) }
+  getOnlineUsers() { return this.http.get(host + '/api/getOnlineUsers') };
+  getUserConnectedChats(userId) { return this.http.get(host + '/api/getUserConnectedChats?userId='+userId) }
 
-  addUserToChat(data) {return this.http.get('http://127.0.0.1:2000/api/addUserToChat?userId='+data.userId+'&chatId='+data.chatId) }
+  addUserToChat(data) {return this.http.get(host + '/api/addUserToChat?userId='+data.userId+'&chatId='+data.chatId) }
 
   updateUser(data) {
-    return this.http.post('http://127.0.0.1:2000/api/updateUser?userId='+data.userId, {userName: data.userName}, httpOptions);
+    return this.http.post(host + '/api/updateUser?userId='+data.userId, {userName: data.userName}, httpOptions);
   }
 
   removeUserFromChat(data) {
-    return this.http.get('http://127.0.0.1:2000/api/removeUserFromChat?userId='+data.userId+'&chatId='+data.chatId);
+    return this.http.get(host + '/api/removeUserFromChat?userId='+data.userId+'&chatId='+data.chatId);
   }
 }
